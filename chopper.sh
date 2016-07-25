8cc62d1b404c82d2597c66d9431e42495cc69490
Alan Bush <al@albush.com>
2016-07-25 10:39:55 -0500

    mp3 conversion

--- chopper.sh
+++ chopper.sh
@@ -26,84 +26,83 @@ usage ()
 # ----- Begin getopts -----
 while getopts :f:c:o:AE:h FLAG; do
   case $FLAG in
-    # A)  #set option "a"
-    #   audio=$OPTARG
-    #   # echo "-A used, we'll export an mp3 for you."
-    #   # echo "OPT_A = $audio"
-    #   ;;
+    A)  #set option "a"
+      audio=1
+      echo $audio
+      ;;
     f)  #set option "f"
       filename=$OPTARG
-      # echo "-f used: $OPTARG"
-      # echo "input file name = $filename"
+      if [[ ! -f "$filename" ]]
+      then
+          echo "That video doesn't exist. Try again"
+          exit
+      else echo "Video......... Check!"
+        fbname=$(basename "$filename" .mp4)
+      fi
       ;;
     c)  #set option "c"
       cut_list=$OPTARG
-      # echo "-c used: $OPTARG"
-      # echo "Cut list = $cut_list"
+      if [[ ! -f "$cut_list" ]]
+        then
+        echo "That cut list doesn't exist. Try again"
+          exit
+        else
+          echo "Cut List........ Check!"
+      fi
       ;;
     E)  #set option "E"
       container=$OPTARG
-      # echo "-E used: $OPTARG"
-      # echo "We will export to Cloud Files container = $container"
       ;;
     o)  #set option "o"
       out_path=$OPTARG
-      # echo "-o used: $OPTARG"
-      # echo "Output path = $out_path"
+      # does the output directory exist?
+        if [ ! -d "$out_path" ]; then
+          while true; do
+            read -p "Oh noes, that directory isn't there. should I create $out_path for you?" yn
+            case $yn in
+                [Yy]* ) mkdir $out_path; break;;
+                [Nn]* ) echo "Ok. I'll be here if you want to try again."; exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+          done
+        fi
       ;;
     h)  #show help
       usage
       ;;
     \?) #unrecognized option - show help
-      echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
-      usage
+      echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."; usage >&2; exit
       ;;
   esac
 done
-
+if [ $OPTIND -eq 1 ]; then echo "No options were passed"; usage; exit; fi
 shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
 
 # ----- End getopts -----
 
-# does the output directory exist?
-  if [ ! -d "$out_path" ]; then
-    while true; do
-      read -p "Oh noes, that directory isn't there. should I create $out_path for you?" yn
-      case $yn in
-          [Yy]* ) mkdir $out_path; break;;
-          [Nn]* ) echo "Ok. I'll be here if you want to try again."; exit;;
-          * ) echo "Please answer yes or no.";;
-      esac
-    done
-  fi
-# read in a csv, use those values to create individual files via ffmpeg.
-# Check to see if the video exists
-if [[ -f "$filename" ]]
+# ----- Extract Audio -----
+if [ $audio == 1 ]
   then
-# now check to see if the cut_list exists
-    echo "Video......... Check!"
-    if [[ -f "$cut_list" ]]
-# The cut list exists, let's chop it up.
-      then
-        echo "Cut List........ Check!"
-# chop the videos
-  while IFS=, read start_time end_time clip_title
-  do
-    echo "Chopping $clip_title"
-    echo "nohup ffmpeg -i $filename -ss $start_time -to $end_time -strict -2 ${out_path%/}/${clip_title// /_}.mp4 < /dev/null &"
-    echo "$clip_title Finished"
-  done < $cut_list
-# if the cut list does not exist, exit.
-      else echo "That cut list doesn't exist. Try again"
-        exit
-    fi
-# if the cut video does not exist, exit.
-  else echo "That video doesn't exist. Try again"
-    exit
+    echo "Extracting Audio from $filename"
+    ffmpeg -i $filename  -strict -2 ${out_path%/}/${fbname// /_}.mp3
+    # ffmpeg -i $filename  -strict -2 ${out_path%/}/${fbname// /_}.ogg 
+    echo "$filename Audio Extraction Finished"
 fi
+# ----- End Audio Extraction -----
+
+# read in a csv, use those values to create individual files via ffmpeg.
+# chop the videos
+while IFS=, read start_time end_time clip_title
+do
+  echo "Chopping $clip_title"
+  nohup ffmpeg -i $filename -ss $start_time -to $end_time -strict -2 ${out_path%/}/${clip_title// /_}.mp4 < /dev/null &
+  echo "$clip_title Finished"
+done < $cut_list
+
+# Optional Export
 
 if [ ! -z "$container" ]
   then
     echo "Let's upload these files. Shipping them off to your container named $container."
-    echo "turbolift --os-endpoint-type internalURL -u $OS_USERNAME -a $OS_API_KEY --os-rax-auth $OS_RAX_AUTH upload -s $clip_path -c" $container
+    turbolift --os-endpoint-type internalURL -u $OS_USERNAME -a $OS_API_KEY --os-rax-auth $OS_RAX_AUTH upload -s $clip_path -c $container
 fi
